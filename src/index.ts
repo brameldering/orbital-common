7900b4e9029e181edd7a8b585892b7f9f979fb04
Bram Eldering <bram.eldering@gmail.com>
2023-10-30 11:48:57 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export * from './types/request-types';
 export * from './types/product-types';
 export * from './types/user-types';
 
+export * from './middleware/auth-middleware';
+export * from './middleware/checkObjectId';
 export * from './middleware/error-handler';
 export * from './middleware/validate-request';
-export * from './middleware/auth-middleware';
-export * from './middleware/auth-middleware';
