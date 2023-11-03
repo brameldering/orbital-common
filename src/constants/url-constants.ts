7b6c909f4147ac71b700d3230f08c07818616cf1
Bram Eldering <bram.eldering@gmail.com>
2023-11-03 18:14:20 +0100

    Updates

--- src/constants/url-constants.ts
+++ src/constants/url-constants.ts
@@ -7,13 +7,10 @@ export const ID_SEQUENCE_INVOICES_URL = '/api/idsequence/v2/invoices';
 
 // Auth / Users
 export const USERS_URL = '/api/users/v2';
-
 export const SIGN_UP_URL = USERS_URL + '/signup';
 export const SIGN_IN_URL = USERS_URL + '/signin';
 export const SIGN_OUT_URL = USERS_URL + '/signout';
-
 export const CURRENT_USER_URL = USERS_URL + '/currentuser';
-
 export const RESET_PASSWORD_URL = USERS_URL + '/resetpassword';
 export const UPDATE_PASSWORD_URL = USERS_URL + '/updatepassword';
 export const UPDATE_PROFILE_URL = USERS_URL + '/profile';
