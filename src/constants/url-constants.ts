2c09a14c5f74335dc99f42028ca18dfcb19a1fb2
Bram Eldering <bram.eldering@gmail.com>
2023-11-19 19:59:31 +0100

    Updates

--- src/constants/url-constants.ts
+++ src/constants/url-constants.ts
@@ -18,6 +18,7 @@ export const RESET_PASSWORD_URL = USERS_URL + '/resetpassword';
 export const UPDATE_PASSWORD_URL = USERS_URL + '/updatepassword';
 export const UPDATE_PROFILE_URL = USERS_URL + '/profile';
 export const USER_ROLES_URL = USERS_URL + '/roles';
+export const TEMP_URL = USERS_URL + '/temp';
 
 // Products
 export const PRODUCTS_URL = '/api/products/v2';
