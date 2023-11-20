0ae8dcfde28e1f949fd40463cc05545036e9cb96
Bram Eldering <bram.eldering@gmail.com>
2023-11-20 20:53:19 +0100

    Updates

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -61,6 +61,7 @@ export const authorize = (
 
   const result = getAccessByApiAndMethod(apiUrl, apiMethod);
   console.log(result);
+  next();
   /*
   if (!req.currentUser) {
     // console.log('protect: !req.currentUser error');
