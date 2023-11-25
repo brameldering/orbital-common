c4edb7327d815b564fcba0e7178a981659efaea0
Bram Eldering <bram.eldering@gmail.com>
2023-11-25 15:19:00 +0100

    Updates

--- src/middleware/check-object-id.ts
+++ src/middleware/check-object-id.ts
@@ -3,11 +3,13 @@ import { Request, Response, NextFunction } from 'express';
 import { isValidObjectId } from 'mongoose';
 import { UserInputError } from '../types/error-types';
 
-function checkObjectId(req: Request, res: Response, next: NextFunction) {
+export const checkObjectId = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (!isValidObjectId(req.params.id)) {
     throw new UserInputError(`Invalid ObjectId: ${req.params.id}`);
   }
   next();
-}
-
-export default checkObjectId;
+};
