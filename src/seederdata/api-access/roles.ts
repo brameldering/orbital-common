638a8b45a81f474391c88138562659f2d52e79e3
Bram Eldering <bram.eldering@gmail.com>
2023-12-05 11:19:46 +0100

    Updates

--- src/seederdata/api-access/roles.ts
+++ src/seederdata/api-access/roles.ts
@@ -2,6 +2,6 @@ import { IRoleObj } from '../../types/mongoose-model-types/mongoose-access-types
 import { CUSTOMER_ROLE, ADMIN_ROLE } from '../../constants/role-constants';
 
 export const roles: IRoleObj[] = [
-  { role: CUSTOMER_ROLE },
-  { role: ADMIN_ROLE },
+  { role: CUSTOMER_ROLE, roleDisplay: 'Customer' },
+  { role: ADMIN_ROLE, roleDisplay: 'Administrator' },
 ];
