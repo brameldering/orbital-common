b528045ebc9769110b4e9ff5bc1cfc147546bba2
Bram Eldering <bram.eldering@gmail.com>
2023-12-09 14:59:23 +0100

    Updates

--- src/constants/access/microservice-names.ts
+++ src/constants/access/microservice-names.ts
@@ -1,11 +1,19 @@
-export const MICROSERVICE_AUTH: string = 'auth-microservice';
-export const MICROSERVICE_PRODUCTS: string = 'products-microservice';
-export const MICROSERVICE_ORDERS: string = 'orders-microservice';
-export const MICROSERVICE_SEQUENCES: string = 'seq-service-microservice';
-export const MICROSERVICE_SEEDER: string = 'seeder-microservice';
+export const MICROSERVICE_AUTH: string = 'auth';
+export const MICROSERVICE_PRODUCTS: string = 'products';
+export const MICROSERVICE_ORDERS: string = 'orders';
+export const MICROSERVICE_SEEDER: string = 'seed';
 
-export const MICROSERVICE_CUSTOMER: string = 'customer-microservice';
-export const MICROSERVICE_INVENTORY: string = 'inventory-microservice';
-export const MICROSERVICE_PAYMENT: string = 'payment-microservice';
-export const MICROSERVICE_CHARGE: string = 'charge-microservice';
-export const MICROSERVICE_INVOICE: string = 'invoice-microservice';
+export const CURRENT_MICROSERVICES = [
+  MICROSERVICE_AUTH,
+  MICROSERVICE_PRODUCTS,
+  MICROSERVICE_ORDERS,
+  MICROSERVICE_SEEDER,
+];
+
+export const MICROSERVICE_SEQUENCES: string = 'seq';
+
+export const MICROSERVICE_CUSTOMER: string = 'customer';
+export const MICROSERVICE_INVENTORY: string = 'inventory';
+export const MICROSERVICE_PAYMENT: string = 'payment';
+export const MICROSERVICE_CHARGE: string = 'charge';
+export const MICROSERVICE_INVOICE: string = 'invoice';
