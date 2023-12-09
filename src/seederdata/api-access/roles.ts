026879376cf5027e63dffface92ba85b49efda5f
Bram Eldering <bram.eldering@gmail.com>
2023-12-09 08:41:12 +0100

    Updates

--- src/seederdata/api-access/roles.ts
+++ src/seederdata/api-access/roles.ts
@@ -1,5 +1,7 @@
 import { IRoleObj } from '../../types/mongoose-model-types/mongoose-access-types';
 import {
+  ANONYMOUS_ROLE,
+  ANONYMOUS_DISPLAY,
   CUSTOMER_ROLE,
   CUSTOMER_DISPLAY,
   ADMIN_ROLE,
@@ -7,6 +9,7 @@ import {
 } from '../../constants/role-constants';
 
 export const roles: IRoleObj[] = [
+  { role: ANONYMOUS_ROLE, roleDisplay: ANONYMOUS_DISPLAY },
   { role: CUSTOMER_ROLE, roleDisplay: CUSTOMER_DISPLAY },
   { role: ADMIN_ROLE, roleDisplay: ADMIN_DISPLAY },
 ];
