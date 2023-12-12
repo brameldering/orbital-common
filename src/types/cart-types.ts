6ba07d4926ead517d3dc87fd337e0ed9b2a33331
Bram Eldering <bram.eldering@gmail.com>
2023-12-12 15:42:40 +0100

    Updates

--- src/types/cart-types.ts
+++ src/types/cart-types.ts
@@ -7,8 +7,16 @@ export interface ICart {
   paymentMethod: string;
   totalAmounts: ITotalAmounts;
 }
-
 export interface ICartItem {
+  productId: string;
+  productName: string;
+  imageURL: string;
+  price: number;
+  countInStock: number;
+  qty: number;
+}
+
+export interface ICartItemWithSettings {
   productId: string;
   productName: string;
   imageURL: string;
