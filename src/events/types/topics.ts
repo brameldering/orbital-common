5e7bda30b5e9596fb7ea89595cc1438e8c6f273b
Bram Eldering <bram.eldering@gmail.com>
2023-12-15 17:38:24 +0100

    Updates

--- src/events/types/topics.ts
+++ src/events/types/topics.ts
@@ -1,5 +1,5 @@
 export enum Topics {
-  ApiAccessCreated = 'apiAccess-created',
-  ApiAccessUpdated = 'apiAccess-updated',
-  ApiAccessDeleted = 'apiAccess-deleted',
+  ApiAccessCreated = 'apiaccess-created',
+  ApiAccessUpdated = 'apiaccess-updated',
+  ApiAccessDeleted = 'apiaccess-deleted',
 }
