5843c4690c75058e377b5b0e6ac1ffc6674d4348
Bram Eldering <bram.eldering@gmail.com>
2023-12-20 11:49:40 +0100

    Updates

--- src/utils/get-kafka-log-level.ts
+++ src/utils/get-kafka-log-level.ts
@@ -1,10 +1,20 @@
 import { logLevel } from 'kafkajs';
 
+const logLevelKeys = Object.keys(logLevel)
+  .filter((key) => isNaN(Number(key))) // Filter out numeric keys
+  .map((key) => `'${key}'`); // Map to format each key as 'Key'
+
+const commaSeparatedString = logLevelKeys.join(', ');
+
 export const getKafkaLogLevel = (level: string): logLevel => {
   let logLvl = logLevel[level as keyof typeof logLevel];
   if (!logLvl) {
-    console.error(`getLogLevel Error, level string: ${level} not valid`);
-    throw Error(`getLogLevel Error, level string: ${level} not valid`);
+    console.error(
+      `getLogLevel Error, log level: ${level} not valid, valid values are: ${commaSeparatedString}`
+    );
+    throw Error(
+      `getLogLevel Error, log level: ${level} not valid, valid values are: ${commaSeparatedString}`
+    );
   }
   return logLvl;
 };
