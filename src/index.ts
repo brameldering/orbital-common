7378d56b329820e7e06a5a31a0f752f0ea024750
Bram Eldering <bram.eldering@gmail.com>
2023-12-25 11:39:51 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,56 @@
 export * from './constants/access/apis-auth';
 export * from './constants/access/apis-orders';
 export * from './constants/access/apis-products';
-// export * from './constants/access/old/apis-seq-service';
 export * from './constants/access/microservice-names';
 export * from './constants/payment-methods';
-export * from './constants/url-constants';
 export * from './constants/role-constants';
 export * from './constants/test-constants';
+export * from './constants/url-constants';
+
+export * from './kafka/types/api-access/api-access-created-event';
+export * from './kafka/types/api-access/api-access-updated-event';
+export * from './kafka/types/api-access/api-access-deleted-event';
+export * from './kafka/types/sequence/sequence-request-event';
+export * from './kafka/types/sequence/sequence-response-event';
+export * from './kafka/types/consumer-config';
+export * from './kafka/types/order-status';
+export * from './kafka/types/topics';
+export * from './kafka/base-listener';
+export * from './kafka/base-publisher';
+export * from './kafka/kafka-wrapper';
+export * from './kafka/listener-manager';
+
+export * from './middleware/api-access-cache';
+export * from './middleware/auth-middleware';
+export * from './middleware/cache-middleware';
+export * from './middleware/check-object-id';
+export * from './middleware/error-handler';
+export * from './middleware/validate-request';
+export * from './middleware/validate-url';
+
+export * from './models/access/api-access-model';
+export * from './models/access/role-model';
+export * from './models/order/order-contact-model';
+export * from './models/order/order-item-model';
+export * from './models/order/order-model';
+export * from './models/order/order-sequence-model'; // To remove
+export * from './models/order/order-total-amounts-model';
+export * from './models/order/price-calc-settings-model';
+export * from './models/product/product-model';
+export * from './models/product/product-review-model';
+export * from './models/product/product-sequence-model'; // To remove
+export * from './models/sequence/sequence-model';
+export * from './models/user/user-model';
+
+export * from './seederdata/api-access/api-access-all';
+export * from './seederdata/api-access/roles';
 
 export * from './types/mongoose-model-types/mongoose-access-types';
-export * from './types/mongoose-model-types/mongoose-user-types';
-export * from './types/mongoose-model-types/mongoose-product-types';
 export * from './types/mongoose-model-types/mongoose-order-types';
 export * from './types/mongoose-model-types/mongoose-price-calc-settings-types';
+export * from './types/mongoose-model-types/mongoose-product-types';
+export * from './types/mongoose-model-types/mongoose-sequence-types';
+export * from './types/mongoose-model-types/mongoose-user-types';
 export * from './types/api-access-types';
 export * from './types/cart-types';
 export * from './types/common-types';
@@ -25,41 +63,6 @@ export * from './types/request-types';
 export * from './types/role-types';
 export * from './types/user-types';
 
-export * from './middleware/api-access-cache';
-export * from './middleware/auth-middleware';
-export * from './middleware/cache-middleware';
-export * from './middleware/check-object-id';
-export * from './middleware/error-handler';
-export * from './middleware/validate-request';
-export * from './middleware/validate-url';
-
 export * from './utils/calc-prices';
 export * from './utils/get-kafka-log-level';
 export * from './utils/wait';
-
-export * from './models/access/api-access-model';
-export * from './models/access/role-model';
-export * from './models/order/order-contact-model';
-export * from './models/order/order-item-model';
-export * from './models/order/order-model';
-export * from './models/order/order-sequence-model';
-export * from './models/order/order-total-amounts-model';
-export * from './models/order/price-calc-settings-model';
-export * from './models/product/product-model';
-export * from './models/product/product-sequence-model';
-export * from './models/product/product-review-model';
-export * from './models/user/user-model';
-
-export * from './kafka/kafka-wrapper';
-export * from './kafka/base-listener';
-export * from './kafka/base-publisher';
-export * from './kafka/listener-manager';
-export * from './kafka/types/topics';
-export * from './kafka/types/order-status';
-export * from './kafka/types/consumer-config';
-export * from './kafka/types/api-access/api-access-created-event';
-export * from './kafka/types/api-access/api-access-updated-event';
-export * from './kafka/types/api-access/api-access-deleted-event';
-
-export * from './seederdata/api-access/roles';
-export * from './seederdata/api-access/api-access-all';
