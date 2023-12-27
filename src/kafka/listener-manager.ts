6ebf8a61dd3a6b269ec9f037f3a4dccdc3de203c
Bram Eldering <bram.eldering@gmail.com>
2023-12-27 11:46:41 +0100

    Updates

--- src/kafka/listener-manager.ts
+++ src/kafka/listener-manager.ts
@@ -4,7 +4,7 @@ import { IConsumerConfig } from './types/consumer-config';
 import { ApplicationIntegrityError } from '../types/error-types';
 
 export class ListenerManager {
-  private consumer?: Consumer;
+  private consumer: Consumer;
   private listeners: Map<string, Listener<any>> = new Map();
 
   constructor(
@@ -13,29 +13,16 @@ export class ListenerManager {
     protected consumerConfig: IConsumerConfig
   ) {
     console.log(`Creating consumer for CG ${consumerGroupID}...`);
-    try {
-      this.consumer = client.consumer({
-        groupId: consumerGroupID,
-        ...consumerConfig,
-      });
-      console.log(`Created consumer for CG ${consumerGroupID}`);
-    } catch (error: any) {
-      console.error(
-        `Error creating consumer for CG ${this.consumerGroupID}`,
-        error
-      );
-      throw new Error(`Error creating consumer for CG ${this.consumerGroupID}`);
-    }
+    this.consumer = client.consumer({
+      groupId: consumerGroupID,
+      ...consumerConfig,
+    });
+    console.log(`Created consumer for CG ${consumerGroupID}`);
   }
 
   async connect() {
     console.log(`Connecting consumer for CG ${this.consumerGroupID}...`);
     try {
-      if (!this.consumer) {
-        throw new Error(
-          'Cannot get consumer, create consumer first using the listener-manager constructor'
-        );
-      }
       await this.consumer.connect();
       console.log(`Connected consumer for CG ${this.consumerGroupID}`);
     } catch (error: any) {
@@ -52,11 +39,6 @@ export class ListenerManager {
   async disconnect() {
     console.log(`Disconnecting consumer for CG ${this.consumerGroupID}...`);
     try {
-      if (!this.consumer) {
-        throw new Error(
-          'Error disconnecting consumer, consumer is not defined'
-        );
-      }
       await this.consumer.disconnect();
       console.log(`Disconnected consumer for CG ${this.consumerGroupID}`);
     } catch (error: any) {
@@ -77,11 +59,6 @@ export class ListenerManager {
     try {
       this.listeners.set(listener.topic, listener);
       // Subscribe the consumer to the topic
-      if (!this.consumer) {
-        throw new Error(
-          'Error subscribing consumer, create consumer first using the listener-manager constructor and connect'
-        );
-      }
       await this.consumer.subscribe({
         topic: listener.topic,
         fromBeginning: true,
@@ -101,62 +78,45 @@ export class ListenerManager {
   }
 
   async listen() {
-    try {
-      if (!this.consumer) {
-        throw new Error(
-          'Error in setting up listening for consumer, create consumer first using the listener-manager constructor, connect and subscribe'
-        );
-      }
-      await this.consumer.run({
-        eachMessage: async ({
-          topic,
-          partition,
-          message,
-        }: EachMessagePayload) => {
-          const listener = this.listeners.get(topic);
-          if (listener) {
-            // Extracting the key from the message
-            const keyBuffer = message.key;
-            let keyString = '';
-            // Convert the key from Buffer to string, if the key exists
-            if (keyBuffer) {
-              keyString = keyBuffer.toString();
-            } else {
-              throw new ApplicationIntegrityError(
-                `Error in listenerManager.listen: message.key has not been given for topic ${topic}`
-              );
-            }
-            const parsedData = listener.parseMessage(message);
-            console.log(
-              `Received message for CG: ${this.consumerGroupID}, topic: ${topic}, partition: ${partition}, offset: ${message.offset}, key: ${keyString} - data:`,
-              parsedData
-            );
-            await listener.onMessage(keyString, parsedData);
+    await this.consumer.run({
+      eachMessage: async ({
+        topic,
+        partition,
+        message,
+      }: EachMessagePayload) => {
+        const listener = this.listeners.get(topic);
+        if (listener) {
+          // Extracting the key from the message
+          const keyBuffer = message.key;
+          let keyString = '';
+          // Convert the key from Buffer to string, if the key exists
+          if (keyBuffer) {
+            keyString = keyBuffer.toString();
           } else {
             throw new ApplicationIntegrityError(
-              `Error in listenerManager.listen: listener is not defined for topic ${topic}`
+              `Error in listenerManager.listen: message.key has not been given for topic ${topic}`
             );
           }
-        },
-      });
-    } catch (error: any) {
+          const parsedData = listener.parseMessage(message);
+          console.log(
+            `Received message for CG: ${this.consumerGroupID}, topic: ${topic}, partition: ${partition}, offset: ${message.offset}, key: ${keyString} - data:`,
+            parsedData
+          );
+          await listener.onMessage(keyString, parsedData);
+        } else {
+          throw new ApplicationIntegrityError(
+            `Error in listenerManager.listen: listener is not defined for topic ${topic}`
+          );
+        }
+      },
+    });
+
+    this.consumer.on('consumer.crash', ({ payload }) => {
       console.error(
-        `Error in listenerManager.listen for CG ${this.consumerGroupID}`,
-        error
+        `=> consumer.crash in CG: ${this.consumerGroupID}:`,
+        payload.error
       );
-      throw new Error(
-        `Error in listenerManager.listen for CG ${this.consumerGroupID}`
-      );
-    }
-    // To do, improve error handling
-    if (this.consumer) {
-      this.consumer.on('consumer.crash', ({ payload }) => {
-        console.error(
-          `=> consumer.crash in CG: ${this.consumerGroupID}:`,
-          payload.error
-        );
-        throw new Error(`Error in consumer for CG ${this.consumerGroupID}`);
-      });
-    }
+      throw new Error(`Error in consumer for CG ${this.consumerGroupID}`);
+    });
   }
 }
