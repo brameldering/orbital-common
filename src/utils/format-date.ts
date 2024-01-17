b89a93ed7c38d702c1c356ecca974910ae8eac06
Bram Eldering <bram.eldering@gmail.com>
2024-01-17 18:22:34 +0100

    Updates

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,5 +1,9 @@
 // Return a string in format dd-mm-yyyy for a Date parameter
 export const formatDate = (date: Date): string => {
+  if (!(date instanceof Date)) {
+    throw new Error('Invalid date');
+  }
+
   const day = date.getDate();
   const month = date.getMonth() + 1; // Months are zero-indexed
   const year = date.getFullYear();
